b9be93a1593c555ea4f022e9544a5f0ae675bc2a
Siddharth Kumar Yadav <siddharthsk1234@gmail.com>
2022-06-06 18:36:21 +0530

    [6/6/2022]

--- baseConversion.cpp
+++ baseConversion.cpp
@@ -3,7 +3,7 @@
 #pragma GCC optimize("-Ofast,unroll-loops")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 
-#include <iostream>
+#include <iost
 #include <algorithm>
 #include <functional>
 #include <cmath>
