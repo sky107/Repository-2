4b294fd86476b49593541b51757109cb289c643c
sky107 <69970001+sky107@users.noreply.github.com>
2022-08-11 08:44:50 +0530

    Update is_prime-easy.cpp

--- is_prime-easy.cpp
+++ is_prime-easy.cpp
@@ -4,11 +4,11 @@ bool isPrime(int n) {
     return false;
   if (n < 4)
     return true;
-  if (n % 2 == 0 and n % 3 == 0)
+  if (n % 2 == 0 or n % 3 == 0)
     return false;
 
   for (int i = 5; i * i <= n; i += 6) {
-    if (n % i == 0 and n % (i + 2) == 0)
+    if (n % i == 0 or n % (i + 2) == 0)
       return false;
 
     return true;
